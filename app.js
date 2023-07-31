b87c9d04cee6795db561003fc27c46bed91f6b2b
KevoWaZ <kevin.gauthier145@gmail.com>
2023-07-31 23:06:10 +0200

    SAS ASA
    Search SAS ASA

--- app.js
+++ app.js
@@ -9,8 +9,10 @@ let hauteurB = document.getElementById("hauteurB")
 let hauteurC = document.getElementById("hauteurC")
 let aire = document.getElementById("aire")
 let btnAngles = document.getElementById("calcul2Angles")
+let btn3C = document.getElementById("calcul3C")
 let btnSAS = document.getElementById("calculSAS")
-let btnSSA = document.getElementById("calculSSA")
+let btnASA = document.getElementById("calculASA")
+let btnChercher = document.getElementById("chercher")
 let btnReset = document.getElementById("reset")
 
 
@@ -39,22 +41,14 @@ function coteAF() {
         let angleGammaDegrees = (angleGammaRadians * 180) / Math.PI;
         angleGamma.value = angleGammaDegrees.toFixed(3);
 
-        hauteurA.value = coteB.value * Math.sin(angleGammaDegrees * Math.PI / 180); // Convertir l'angle Gamma en radians avant de calculer le sin
-        hauteurB.value = coteB.value * Math.sin(angleBetaDegrees * Math.PI / 180);  // ERREUR JE DOIS MODIFIER
-        hauteurC.value = coteB.value * Math.sin(angleAlphaDegrees * Math.PI / 180);
+        hauteurA.value = coteB.value * Math.sin(angleGammaDegrees * Math.PI / 180).toFixed(3); // Convertir l'angle Gamma en radians avant de calculer le sin
+        hauteurB.value = coteB.value * Math.sin(angleBetaDegrees * Math.PI / 180).toFixed(3);  // ERREUR JE DOIS MODIFIER
+        hauteurC.value = coteB.value * Math.sin(angleAlphaDegrees * Math.PI / 180).toFixed(3);
 
-        aire.value = (coteA.value * hauteurA.value) / 2
+        aire.value = ((coteA.value * hauteurA.value) / 2).toFixed(3)
 }
 
-coteA.onchange = function () {
-    coteAF()
-}
-
-coteB.onchange = function () {
-    coteAF()
-}
-
-coteC.onchange = function () {
+btn3C.onclick = function () {
     coteAF()
 }
 
@@ -100,13 +94,13 @@ btnAngles.onclick = function () {
 }
 
 
-// // TROUVER A PARTIR DU COTE ANGLE COTE CAS SAS
+// TROUVER A PARTIR DU COTE ANGLE COTE CAS SAS Un angle et les deux côtés adjacents
 
 
 function SAS_A_Gamma_B() {
         let angleGammaRadiansSAS = (angleGamma.value * Math.PI) / 180
 
-        coteC.value = (Math.sqrt(coteA.value ** 2 + coteB.value ** 2 - 2 * coteA.value * coteB.value * Math.cos(angleGammaRadiansSAS))).toFixed(2)
+        coteC.value = (Math.sqrt(coteA.value ** 2 + coteB.value ** 2 - 2 * coteA.value * coteB.value * Math.cos(angleGammaRadiansSAS))).toFixed(3)
 
 
         let cosAlpha = (coteB.value ** 2 + coteC.value ** 2 - coteA.value ** 2) / (2 * coteB.value * coteC.value)
@@ -119,11 +113,11 @@ function SAS_A_Gamma_B() {
         let angleBetaDegrees = (angleBetaRadians * 180) / Math.PI;
         angleBeta.value = angleBetaDegrees.toFixed(3);
 
-        hauteurA.value = coteB.value * Math.sin(angleGammaRadiansSAS);
-        hauteurB.value = coteB.value * Math.sin(angleBetaDegrees * Math.PI / 180);
-        hauteurC.value = coteB.value * Math.sin(angleAlphaDegrees * Math.PI / 180);
+        hauteurA.value = coteB.value * Math.sin(angleGammaRadiansSAS).toFixed(3);
+        hauteurB.value = coteB.value * Math.sin(angleBetaDegrees * Math.PI / 180).toFixed(3);
+        hauteurC.value = coteB.value * Math.sin(angleAlphaDegrees * Math.PI / 180).toFixed(3);
 
-        aire.value = (coteA.value * hauteurA.value) / 2
+        aire.value = ((coteA.value * hauteurA.value) / 2).toFixed(3)
 
 }
 
@@ -131,7 +125,7 @@ function SAS_A_Gamma_B() {
 function SAS_B_Alpha_C() {
         let angleAlphaRadiansSAS = (angleAlpha.value * Math.PI) / 180
 
-        coteA.value = (Math.sqrt(coteB.value ** 2 + coteC.value ** 2 - 2 * coteB.value * coteC.value * Math.cos(angleAlphaRadiansSAS))).toFixed(2)
+        coteA.value = (Math.sqrt(coteB.value ** 2 + coteC.value ** 2 - 2 * coteB.value * coteC.value * Math.cos(angleAlphaRadiansSAS))).toFixed(3)
 
 
         let cosAlpha = (coteB.value ** 2 + coteC.value ** 2 - coteA.value ** 2) / (2 * coteB.value * coteC.value)
@@ -149,18 +143,18 @@ function SAS_B_Alpha_C() {
         let angleGammaDegrees = (angleGammaRadians * 180) / Math.PI;
         angleGamma.value = angleGammaDegrees.toFixed(3);
 
-        hauteurA.value = coteB.value * Math.sin(angleGammaDegrees * Math.PI / 180);
-        hauteurB.value = coteB.value * Math.sin(angleBetaDegrees * Math.PI / 180);
-        hauteurC.value = coteB.value * Math.sin(angleAlphaDegrees * Math.PI / 180);
+        hauteurA.value = coteB.value * Math.sin(angleGammaDegrees * Math.PI / 180).toFixed(3);
+        hauteurB.value = coteB.value * Math.sin(angleBetaDegrees * Math.PI / 180).toFixed(3);
+        hauteurC.value = coteB.value * Math.sin(angleAlphaDegrees * Math.PI / 180).toFixed(3);
 
-        aire.value = (coteA.value * hauteurA.value) / 2
+        aire.value = ((coteA.value * hauteurA.value) / 2).toFixed(3)
 }
 
 
 function SAS_C_Beta_A() {
         let angleBetaRadiansSAS = (angleBeta.value * Math.PI) / 180
 
-        coteB.value = (Math.sqrt(coteC.value ** 2 + coteA.value ** 2 - 2 * coteC.value * coteA.value * Math.cos(angleBetaRadiansSAS))).toFixed(2)
+        coteB.value = (Math.sqrt(coteC.value ** 2 + coteA.value ** 2 - 2 * coteC.value * coteA.value * Math.cos(angleBetaRadiansSAS))).toFixed(3)
 
 
         let cosAlpha = (coteB.value ** 2 + coteC.value ** 2 - coteA.value ** 2) / (2 * coteB.value * coteC.value)
@@ -178,15 +172,15 @@ function SAS_C_Beta_A() {
         let angleGammaDegrees = (angleGammaRadians * 180) / Math.PI;
         angleGamma.value = angleGammaDegrees.toFixed(3);
 
-        hauteurA.value = coteB.value * Math.sin(angleGammaDegrees * Math.PI / 180);
-        hauteurB.value = coteB.value * Math.sin(angleBetaDegrees * Math.PI / 180);
-        hauteurC.value = coteB.value * Math.sin(angleAlphaDegrees * Math.PI / 180);
+        hauteurA.value = coteB.value * Math.sin(angleGammaDegrees * Math.PI / 180).toFixed(3);
+        hauteurB.value = coteB.value * Math.sin(angleBetaDegrees * Math.PI / 180).toFixed(3);
+        hauteurC.value = coteB.value * Math.sin(angleAlphaDegrees * Math.PI / 180).toFixed(3);
 
-        aire.value = (coteA.value * hauteurA.value) / 2
+        aire.value = ((coteA.value * hauteurA.value) / 2).toFixed(3)
 }
 
 
-btnSAS.onclick = function () {
+btnSAS.onclick = function SAS_Search() {
     const sideAValue = parseFloat(coteA.value);
     const sideBValue = parseFloat(coteB.value);
     const sideCValue = parseFloat(coteC.value);
@@ -213,96 +207,118 @@ btnSAS.onclick = function () {
 }
 
 
+// // TROUVER A PARTIR de Deux angles et le côté commun CAS ASA
 
-// TROUVER A PARTIR DE COTE COTE ANGLE
+function ASA_Beta_C_Alpha() {
+        
+        angleGamma.value = 180 - angleAlpha.value - angleBeta.value
 
-function SSA_A_B_Gamma() {
-        let angleGammaRadiansSSA = (angleGamma.value * Math.PI) / 180
+        let coteAvalue = (coteC.value * Math.sin((angleAlpha.value * Math.PI) / 180)) / Math.sin((angleGamma.value * Math.PI) / 180)
+        coteA.value = coteAvalue.toFixed(3)
 
-        coteC.value = (Math.sqrt(coteA.value ** 2 + coteB.value ** 2 - 2 * coteA.value * coteB.value * Math.cos(angleGammaRadiansSSA))).toFixed(3)
+        let coteBvalue = (coteC.value * Math.sin((angleBeta.value * Math.PI) / 180)) / Math.sin((angleGamma.value * Math.PI) / 180)
+        coteB.value = coteBvalue.toFixed(3)
 
-        let cosAlpha = (coteB.value ** 2 + coteC.value ** 2 - coteA.value ** 2) / (2 * coteB.value * coteC.value)
-        let angleAlphaRadians = Math.acos(cosAlpha)
-        let angleAlphaDegrees = (angleAlphaRadians * 180) / Math.PI
-        angleAlpha.value = angleAlphaDegrees.toFixed(3)
+        hauteurA.value = (coteB.value * Math.sin(angleGamma.value * Math.PI / 180)).toFixed(3)
+        hauteurB.value = (coteB.value * Math.sin(angleBeta.value * Math.PI / 180)).toFixed(3)
+        hauteurC.value = (coteB.value * Math.sin(angleAlpha.value * Math.PI / 180)).toFixed(3)
 
-        let cosBeta = (coteC.value ** 2 + coteA.value ** 2 - coteB.value ** 2) / (2 * coteC.value * coteA.value);
-        let angleBetaRadians = Math.acos(cosBeta);
-        let angleBetaDegrees = (angleBetaRadians * 180) / Math.PI;
-        angleBeta.value = angleBetaDegrees.toFixed(3);
-
-        hauteurA.value = coteB.value * Math.sin(angleGammaRadiansSSA);
-        hauteurB.value = coteB.value * Math.sin(angleBetaDegrees * Math.PI / 180);
-        hauteurC.value = coteB.value * Math.sin(angleAlphaDegrees * Math.PI / 180);
-
-        aire.value = (coteA.value * hauteurA.value) / 2
+        aire.value = ((coteA.value * hauteurA.value) / 2).toFixed(3)
 }
 
-function SSA_A_C_Beta() {
-        let angleBetaRadiansSSA = (angleBeta.value * Math.PI) / 180
+function ASA_Alpha_B_Gamma() {
+        
+        angleBeta.value = 180 - angleAlpha.value - angleGamma.value
 
-        coteB.value = (Math.sqrt(coteA.value ** 2 + coteC.value ** 2 - 2 * coteA.value * coteC.value * Math.cos(angleBetaRadiansSSA))).toFixed(3)
+        let coteCvalue = (coteB.value * Math.sin((angleGamma.value * Math.PI) / 180)) / Math.sin((angleBeta.value * Math.PI) / 180)
+        coteC.value = coteCvalue.toFixed(3)
 
-        let cosAlpha = (coteB.value ** 2 + coteC.value ** 2 - coteA.value ** 2) / (2 * coteB.value * coteC.value)
-        let angleAlphaRadians = Math.acos(cosAlpha)
-        let angleAlphaDegrees = (angleAlphaRadians * 180) / Math.PI
-        angleAlpha.value = angleAlphaDegrees.toFixed(3)
+        let coteAvalue = (coteC.value * Math.sin((angleAlpha.value * Math.PI) / 180)) / Math.sin((angleGamma.value * Math.PI) / 180)
+        coteA.value = coteAvalue.toFixed(3)
 
-        let cosGamma = (coteA.value ** 2 + coteB.value ** 2 - coteC.value ** 2) / (2 * coteA.value * coteB.value);
-        let angleGammaRadians = Math.acos(cosGamma);
-        let angleGammaDegrees = (angleGammaRadians * 180) / Math.PI;
-        angleGamma.value = angleGammaDegrees.toFixed(3);
 
-        hauteurA.value = coteB.value * Math.sin(angleGammaDegrees * Math.PI / 180);
-        hauteurB.value = coteB.value * Math.sin(angleBetaRadiansSSA);
-        hauteurC.value = coteB.value * Math.sin(angleAlphaDegrees * Math.PI / 180);
+        hauteurA.value = (coteB.value * Math.sin(angleGamma.value * Math.PI / 180)).toFixed(3)
+        hauteurB.value = (coteB.value * Math.sin(angleBeta.value * Math.PI / 180)).toFixed(3)
+        hauteurC.value = (coteB.value * Math.sin(angleAlpha.value * Math.PI / 180)).toFixed(3)
 
-        aire.value = (coteA.value * hauteurA.value) / 2
+        aire.value = ((coteA.value * hauteurA.value) / 2).toFixed(3)
 }
 
-function SSA_B_C_Alpha() {
-        let angleAlphaRadiansSSA = (angleAlpha.value * Math.PI) / 180
+function ASA_Gamma_A_Beta() {
+        
+        angleAlpha.value = 180 - angleBeta.value - angleGamma.value
 
-        coteA.value = (Math.sqrt(coteB.value ** 2 + coteC.value ** 2 - 2 * coteB.value * coteC.value * Math.cos(angleAlphaRadiansSSA))).toFixed(3)
+        coteC.value = (coteA.value * Math.sin(angleGamma.value * Math.PI / 180)) / Math.sin(angleAlpha.value * Math.PI / 180);
 
-        let cosBeta = (coteC.value ** 2 + coteA.value ** 2 - coteB.value ** 2) / (2 * coteC.value * coteA.value)
-        let angleBetaRadians = Math.acos(cosBeta)
-        let angleBetaDegrees = (angleBetaRadians * 180) / Math.PI
-        angleBeta.value = angleBetaDegrees.toFixed(3)
+        let coteBvalue = (coteC.value * Math.sin((angleBeta.value * Math.PI) / 180)) / Math.sin((angleGamma.value * Math.PI) / 180)
+        coteB.value = coteBvalue.toFixed(3)
 
-        let cosGamma = (coteA.value ** 2 + coteB.value ** 2 - coteC.value ** 2) / (2 * coteA.value * coteB.value);
-        let angleGammaRadians = Math.acos(cosGamma);
-        let angleGammaDegrees = (angleGammaRadians * 180) / Math.PI;
-        angleGamma.value = angleGammaDegrees.toFixed(3);
 
-        hauteurA.value = coteB.value * Math.sin(angleGammaDegrees * Math.PI / 180);
-        hauteurB.value = coteB.value * Math.sin(angleBetaDegrees * Math.PI / 180);
-        hauteurC.value = coteB.value * Math.sin(angleAlphaRadiansSSA);
+        hauteurA.value = (coteB.value * Math.sin(angleGamma.value * Math.PI / 180)).toFixed(3)
+        hauteurB.value = (coteB.value * Math.sin(angleBeta.value * Math.PI / 180)).toFixed(3)
+        hauteurC.value = (coteB.value * Math.sin(angleAlpha.value * Math.PI / 180)).toFixed(3)
 
-        aire.value = (coteA.value * hauteurA.value) / 2
+        aire.value = ((coteA.value * hauteurA.value) / 2).toFixed(3)
 }
 
-btnSSA.onclick = function () {
-    const sideAValue = parseFloat(coteA.value);
-    const sideBValue = parseFloat(coteB.value);
-    const sideCValue = parseFloat(coteC.value);
-    const angleAlphaValue = parseFloat(angleAlpha.value);
-    const angleBetaValue = parseFloat(angleBeta.value);
-    const angleGammaValue = parseFloat(angleGamma.value);
+btnASA.onclick = function ASA_Search() {
+        const sideAValue = parseFloat(coteA.value);
+        const sideBValue = parseFloat(coteB.value);
+        const sideCValue = parseFloat(coteC.value);
+        const angleAlphaValue = parseFloat(angleAlpha.value);
+        const angleBetaValue = parseFloat(angleBeta.value);
+        const angleGammaValue = parseFloat(angleGamma.value);
 
-    const numberOfSidesEntered = [sideAValue, sideBValue, sideCValue].filter((value) => !isNaN(value)).length;
-    const numberOfAnglesEntered = [angleAlphaValue, angleBetaValue, angleGammaValue].filter((value) => !isNaN(value)).length;
+        const numberOfSidesEntered = [sideAValue, sideBValue, sideCValue].filter((value) => !isNaN(value)).length;
+        const numberOfAnglesEntered = [angleAlphaValue, angleBetaValue, angleGammaValue].filter((value) => !isNaN(value)).length;
 
-    if(numberOfSidesEntered === 2 && numberOfAnglesEntered === 1) {
-        if (!isNaN(sideBValue) && !isNaN(sideCValue) && !isNaN(angleAlphaValue)) {
-            SSA_B_C_Alpha()
-        } else if (!isNaN(sideAValue) && !isNaN(sideCValue) && !isNaN(angleBetaValue)) {
-            SSA_A_C_Beta()
-        } else if (!isNaN(sideAValue) && !isNaN(sideBValue) && !isNaN(angleGammaValue)) {
-            SSA_A_B_Gamma()
+        if(numberOfSidesEntered === 1 && numberOfAnglesEntered === 2) {
+            if (!isNaN(angleAlphaValue) && !isNaN(angleBetaValue) && !isNaN(sideCValue)) {
+                ASA_Beta_C_Alpha()
+            } else if (!isNaN(angleAlphaValue) && !isNaN(angleGammaValue) && !isNaN(sideBValue)) {
+                ASA_Alpha_B_Gamma()
+            } else if (!isNaN(angleGammaValue) && !isNaN(sideAValue) && !isNaN(angleBetaValue)) {
+                ASA_Gamma_A_Beta()
+            }
         }
-    }
 }
+
+
+
+btnChercher.onclick = function() {
+        const sideAValue = parseFloat(coteA.value);
+        const sideBValue = parseFloat(coteB.value);
+        const sideCValue = parseFloat(coteC.value);
+        const angleAlphaValue = parseFloat(angleAlpha.value);
+        const angleBetaValue = parseFloat(angleBeta.value);
+        const angleGammaValue = parseFloat(angleGamma.value);
+
+        const numberOfSidesEntered = [sideAValue, sideBValue, sideCValue].filter((value) => !isNaN(value)).length;
+        const numberOfAnglesEntered = [angleAlphaValue, angleBetaValue, angleGammaValue].filter((value) => !isNaN(value)).length;
+
+        if (numberOfSidesEntered === 2 && numberOfAnglesEntered === 1) {
+            if (!isNaN(sideAValue) && !isNaN(sideBValue) && !isNaN(angleGammaValue)) {
+                SAS_A_Gamma_B();
+            } else if (!isNaN(sideBValue) && !isNaN(sideCValue) && !isNaN(angleAlphaValue)) {
+                SAS_B_Alpha_C();
+            } else if (!isNaN(sideCValue) && !isNaN(sideAValue) && !isNaN(angleBetaValue)) {
+                SAS_C_Beta_A();
+            } else {
+                alert("Les valeurs entrées ne sont pas valides pour résoudre un triangle SAS.");
+            }
+        }
+
+        if(numberOfSidesEntered === 1 && numberOfAnglesEntered === 2) {
+            if (!isNaN(angleAlphaValue) && !isNaN(angleBetaValue) && !isNaN(sideCValue)) {
+                ASA_Beta_C_Alpha()
+            } else if (!isNaN(angleAlphaValue) && !isNaN(angleGammaValue) && !isNaN(sideBValue)) {
+                ASA_Alpha_B_Gamma()
+            } else if (!isNaN(angleGammaValue) && !isNaN(sideAValue) && !isNaN(angleBetaValue)) {
+                ASA_Gamma_A_Beta()
+            }
+        }
+}
+
 
 
 btnReset.onclick = function () {
